53d232736f05f67d9094e0f437ad13a4d240efc2
vickydarlinn <uttapalsangwan@gmail.com>
2024-02-22 21:14:18 +0530

    feat:add cors

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
+const cors = require("cors");
 dotenv.config({});
 
+app.use(cors());
 // Connect to MongoDB
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		console.log("MongoDB connected");
-	})
-	.catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
 
 // Start the server
 // Create an Express application
 const port = process.env.PORT || 6969;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
